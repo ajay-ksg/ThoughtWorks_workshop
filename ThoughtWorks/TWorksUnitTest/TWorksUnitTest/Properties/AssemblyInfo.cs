77ef2e4a2f12b25a4c48ec35db545c866c33169c
Ajay Kumar <ajay.kumar@winshuttle.com>
2021-03-13 07:26:11 +0530

    Added test project

---
+++ ThoughtWorks/TWorksUnitTest/TWorksUnitTest/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("TWorksUnitTest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("WInshuttle")]
+[assembly: AssemblyProduct("TWorksUnitTest")]
+[assembly: AssemblyCopyright("Copyright © WInshuttle 2021")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("fe77b0b8-e6a9-4097-8e46-7e4af43fd6d9")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
